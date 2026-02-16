241dd1be6de01fb7f891e89cdb7af747767b4cdd
Omnia-Ayman2 <166854259+Omnia-Ayman2@users.noreply.github.com>
2026-02-17 00:52:14 +0200

    wish

---
+++ src/app/core/services/wishlist.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WishListService {
+  myHeaders:any={token :localStorage.getItem('userToken')}
+
+private readonly _HttpClient=inject(HttpClient)
+addToWishlist(id:string):Observable<any>{
+ return this._HttpClient.post(`${environment.baseUrl}/api/Wishlist`,{
+"productId":id
+  },{
+    headers:this.myHeaders
+  })
+}
+userWishlist():Observable<any>{
+  return this._HttpClient.get(`${environment.baseUrl}/api/Wishlist`,{
+    headers:this.myHeaders
+  }
+)
+}
+deleteproduct(id:string):Observable<any>{
+  return this._HttpClient.delete(`${environment.baseUrl}/api/Wishlist/6/${id}`,{
+    headers:this.myHeaders
+  }
+)
+}
+}
